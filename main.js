86ff82b6c85b892ad3ce1a310347460d4096f41d
Jay-JYKim91 <jay.jykim91@gmail.com>
2021-11-16 01:12:13 +0900

    edit menu css

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Progressive loading Start
 const observer = lozad('.lozad', {
-    rootMargin: '2000px 0px'
-  });
-  observer.observe();
+  rootMargin: '2000px 0px'
+});
+observer.observe();
 // Progressive loading End
 
 // Display current year in footer Start
@@ -18,29 +18,29 @@ const menuToReadymade = document.getElementsByClassName('menuToReadymade');
 const menuToEducationProcess = document.getElementsByClassName('menuToEducationProcess');
 
 const menuToCustomArray = Object.keys(menuToCustom).map(function(key) {
-  return menuToCustom[key];
+return menuToCustom[key];
 });
 const menuToReadymadeArray = Object.keys(menuToReadymade).map(function(key) {
-  return menuToReadymade[key];
+return menuToReadymade[key];
 });
 const menuToEducationProcessArray = Object.keys(menuToEducationProcess).map(function(key) {
-  return menuToEducationProcess[key];
+return menuToEducationProcess[key];
 });
 
 menuToCustomArray.forEach(item => {
-  item.addEventListener('click', () => {
-    document.getElementById('portfolio__custom').scrollIntoView();
-  })
+item.addEventListener('click', () => {
+  document.getElementById('portfolio__custom').scrollIntoView();
+})
 });
 menuToReadymadeArray.forEach(item => {
-  item.addEventListener('click', () => {
-    document.getElementById('portfolio__readymade').scrollIntoView();
-  })
+item.addEventListener('click', () => {
+  document.getElementById('portfolio__readymade').scrollIntoView();
+})
 });
 menuToEducationProcessArray.forEach(item => {
-  item.addEventListener('click', () => {
-    document.getElementById('portfolio__education-process').scrollIntoView();
-  })
+item.addEventListener('click', () => {
+  document.getElementById('portfolio__education-process').scrollIntoView();
+})
 });
 // Move to portfolio section End
 
@@ -55,96 +55,38 @@ const portfolioReadymadeOffset = document.getElementById('portfolio__readymade')
 const portfolioEducationOffset = document.getElementById('portfolio__education-process').getBoundingClientRect().top + offSet +1000;
 const portfolioFinishOffset = document.getElementById('portfolio__education-process').getBoundingClientRect().bottom + offSet +1000;
 
-console.log(portfolioCustomOffset);
-console.log(portfolioReadymadeOffset);
-console.log(portfolioEducationOffset);
-console.log(portfolioFinishOffset);
-
-// window.addEventListener('scroll', () => {
-//   console.log(window.pageYOffset);
-
-// });
+// console.log(portfolioCustomOffset);
+// console.log(portfolioReadymadeOffset);
+// console.log(portfolioEducationOffset);
+// console.log(portfolioFinishOffset);
 
 window.addEventListener('scroll', () => {
-  let currentOffset = document.documentElement.scrollTop;
-  console.log(currentOffset);
+let currentOffset = document.documentElement.scrollTop;
+console.log(currentOffset);
 
-  if (currentOffset > portfolioFinishOffset) {
-    smallMenu.style.display = 'block';
-    smallMenuCustom.classList.remove('active-menu');
-    smallMenuReadymade.classList.remove('active-menu');
-    smallMenuEducation.classList.remove('active-menu');
-  } else if (currentOffset > portfolioEducationOffset) {
-    smallMenu.style.display = 'block';
-    smallMenuCustom.classList.remove('active-menu');
-    smallMenuReadymade.classList.remove('active-menu');
-    smallMenuEducation.classList.add('active-menu');
-  } else if (currentOffset > portfolioReadymadeOffset) {
-    smallMenu.style.display = 'block';
-    smallMenuCustom.classList.remove('active-menu');
-    smallMenuReadymade.classList.add('active-menu');
-    smallMenuEducation.classList.remove('active-menu');
-  } else if (currentOffset > portfolioCustomOffset) {
-    smallMenu.style.display = 'block';
-    smallMenuCustom.classList.add('active-menu');
-    smallMenuReadymade.classList.remove('active-menu');
-    smallMenuEducation.classList.remove('active-menu');
-  } else if (currentOffset < portfolioCustomOffset) {
-    smallMenu.style.display = 'none';
-  }
+if (currentOffset > portfolioFinishOffset) {
+  smallMenu.style.opacity = '1';
+  smallMenuCustom.classList.remove('active-menu');
+  smallMenuReadymade.classList.remove('active-menu');
+  smallMenuEducation.classList.remove('active-menu');
+} else if (currentOffset > portfolioEducationOffset) {
+  smallMenu.style.opacity = '1';
+  smallMenuCustom.classList.remove('active-menu');
+  smallMenuReadymade.classList.remove('active-menu');
+  smallMenuEducation.classList.add('active-menu');
+} else if (currentOffset > portfolioReadymadeOffset) {
+  smallMenu.style.opacity = '1';
+  smallMenuCustom.classList.remove('active-menu');
+  smallMenuReadymade.classList.add('active-menu');
+  smallMenuEducation.classList.remove('active-menu');
+} else if (currentOffset > portfolioCustomOffset) {
+  smallMenu.style.opacity = '1';
+  smallMenuCustom.classList.add('active-menu');
+  smallMenuReadymade.classList.remove('active-menu');
+  smallMenuEducation.classList.remove('active-menu');
+} else if (currentOffset < portfolioCustomOffset) {
+  smallMenu.style.opacity = '0';
+}
 })
 
 // Display small size of portfolio-menu End
-
-// 포트폴리오 메뉴 작은 버전
-// window.addEventListener('scroll', function() {
-//   let offSet = window.pageYOffset;
-//   // console.log(offSet);
-
-//   let customOffset = document.getElementById('portfolio__custom').getBoundingClientRect().top + offSet;
-//   let readymadeOffset = document.getElementById('portfolio__readymade').getBoundingClientRect().top + offSet;
-//   let processOffset = document.getElementById('portfolio__education-process').getBoundingClientRect().top + offSet;
-//   let finishOffset = document.querySelector('.about').getBoundingClientRect().top + offSet - 700;
-
-//   if (document.documentElement.scrollTop > finishOffset) {
-//     document.getElementById("menu_education-process").style.top = "-80px";
-//     document.getElementById("menu_readymade").style.top = "-80px";
-//     document.getElementById("menu_custom").style.top = "-80px";
-//   } else if (document.documentElement.scrollTop > processOffset) {
-//     document.getElementById("menu_education-process").style.top = "0";
-//   } else if (document.documentElement.scrollTop > readymadeOffset) {
-//     document.getElementById("menu_education-process").style.top = "-80px";
-//     document.getElementById("menu_readymade").style.top = "0";
-//   } else if (document.documentElement.scrollTop > customOffset) {
-//     document.getElementById("menu_readymade").style.top = "-80px";
-//     document.getElementById("menu_custom").style.top = "0";
-//   } else {
-//     document.getElementById("menu_custom").style.top = "-80px";
-//   }
-// });
-
-// console.log(customOffset);
-// console.log(customOffset + window.pageYOffset);
-// console.log(readymadeOffset);
-// console.log(readymadeOffset + window.pageYOffset);
-// console.log(processOffset);
-// console.log(processOffset + window.pageYOffset);
-// console.log(finishOffset);
-
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.documentElement.scrollTop > finishOffset) {
-//     document.getElementById("menu_education-process").style.top = "-80px";
-//   } else if (document.documentElement.scrollTop > processOffset) {
-//     document.getElementById("menu_education-process").style.top = "0";
-//   } else if (document.documentElement.scrollTop > readymadeOffset) {
-//     document.getElementById("menu_education-process").style.top = "-80px";
-//     document.getElementById("menu_readymade").style.top = "0";
-//   } else if (document.documentElement.scrollTop > customOffset) {
-//     document.getElementById("menu_readymade").style.top = "-80px";
-//     document.getElementById("menu_custom").style.top = "0";
-//   } else {
-//     document.getElementById("menu_custom").style.top = "-80px";
-//   }
-// }
